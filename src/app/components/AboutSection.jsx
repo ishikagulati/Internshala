24c7845a06bc172e48493e6c0e6066d0cdbf39a8
Ishika Gulati <ishikagulati1601@gmail.com>
2025-05-22 23:01:04 +0530

    Your commit message

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -54,14 +54,15 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            Hello World, I'm Ishika Gulati, a final-year BTech student in Information Technology at GGSIPU. With a deep passion for coding, I have honed my skills in Java and Data Structures and Algorithms, enabling me to solve complex problems efficiently. My academic journey has been enriched by my dedication to understanding the intricacies of software development.
-            <br /><br />
-            In addition to my core proficiency in Java, I have also gained experience in full-stack development using the MERN stack. This has allowed me to create several well-rounded projects, each demonstrating my ability to build robust, user-friendly applications. Whether it's front-end design or back-end logic, I enjoy the challenge of turning ideas into reality.
-            <br /><br />
-            I am eager to leverage my skills and knowledge to contribute to innovative projects and collaborate with like-minded professionals as I transition into the professional world.
-            <br /><br />
-            So, thank you for gracing my corner of the digital world! Let's embark on a journey together, where innovation meets imagination, and dreams become reality.
-          </p>
+              Hello World, I&#39;m Ishika Gulati, a final-year BTech student in Information Technology at GGSIPU. With a deep passion for coding, I have honed my skills in Java and Data Structures and Algorithms, enabling me to solve complex problems efficiently. My academic journey has been enriched by my dedication to understanding the intricacies of software development.
+  <br /><br />
+  In addition to my core proficiency in Java, I have also gained experience in full-stack development using the MERN stack. This has allowed me to create several well-rounded projects, each demonstrating my ability to build robust, user-friendly applications. Whether it&#39;s front-end design or back-end logic, I enjoy the challenge of turning ideas into reality.
+  <br /><br />
+  I am eager to leverage my skills and knowledge to contribute to innovative projects and collaborate with like-minded professionals as I transition into the professional world.
+  <br /><br />
+  So, thank you for gracing my corner of the digital world! Let&#39;s embark on a journey together, where innovation meets imagination, and dreams become reality.
+</p>
+
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -44,8 +44,8 @@ const EmailSection = () => {
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I'm currently looking for new opportunities, and my inbox is always
-          open. Whether you have a question or just want to say hi, I’ll try my
+          I&#39;m currently looking for new opportunities, and my inbox is always
+          open. Whether you have a question or just want to say hi, I&#39;ll try my
           best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
