202beace93a98226cb544f7592a1f1f8674cd85c
Ishika Gulati <ishikagulati1601@gmail.com>
2025-05-22 23:16:19 +0530

    final

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -42,12 +42,13 @@ const EmailSection = () => {
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I&#39;m currently looking for new opportunities, and my inbox is always
-          open. Whether you have a question or just want to say hi, I&#39;ll try my
-          best to get back to you!
-        </p>
+        <h5 className="text-xl font-bold text-white my-2">Let&apos;s Connect</h5>
+      <p className="text-[#ADB7BE] mb-4 max-w-md">
+  I am currently looking for new opportunities, and my inbox is always open. Whether you have a question or just want to say hi, I will try my best to get back to you!
+</p>
+
+
+
         <div className="socials flex flex-row gap-2">
           <Link href="https://github.com" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
